64e0f9f1a06449d3570361a43e2b68c4b3737c35
CMHeat <84650776+CMHeat@users.noreply.github.com>
2022-05-25 15:29:41 +0800

    added order summary fragment

--- app/src/main/java/com/mobilepalengke/org/MP_CartPage.java
+++ app/src/main/java/com/mobilepalengke/org/MP_CartPage.java
@@ -2,13 +2,29 @@ package com.mobilepalengke.org;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MP_CartPage extends AppCompatActivity {
+
+    Button btnCheckout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mp_cartpage);
+
+        btnCheckout = (Button) findViewById(R.id.btnCheckout);
+        btnCheckout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MP_CartPage.this,MP_CheckoutPage.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
