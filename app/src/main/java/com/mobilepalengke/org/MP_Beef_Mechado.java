ed8668f4ca4f7736a96f666bb2b8adaad34628f6
unknown <navarezjames21@gmail.com>
2022-06-14 10:35:33 +0800

    Add beef mealplan list, add mp beef pare, add beef steak, add beef mechado, add nilagang baka, add text in string.xml

---
+++ app/src/main/java/com/mobilepalengke/org/MP_Beef_Mechado.java
@@ -0,0 +1,14 @@
+package com.mobilepalengke.org;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MP_Beef_Mechado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mp_beef_mechado);
+    }
+}
